ac3b70edb30d11ec1c588aff14ebcb4358b9809b
Vang Xiong <vxiong029@gmail.com>
2019-01-26 13:41:45 -0600

    initial testing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+// react date imports
+import moment from 'moment';
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
 
 class App extends Component {
   state = {
     location: '',
     age: '',
-    startDate: '',
-    endDate: ''
+    startDate: moment(),
+    endDate: null,
+    focusedInput: null,
   }
-  handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('event happended handleSubmit');
+  handleSubmit = () => {
+    console.log(this.state);
   }
   handleChange = propertyName => (event) => {
-    console.log(this.state)
+    // console.log(this.state)
     this.setState({
       [propertyName]: event.target.value,
     });
@@ -48,7 +53,7 @@ class App extends Component {
           </label>
           <br />
           <label>
-            Start Date:
+            {/* Start Date:
               <input
               type="date"
               placeholder="Start Date"
@@ -61,6 +66,15 @@ class App extends Component {
               type="date"
               placeholder="End Date"
               onChange={this.handleChange('endDate')}
+            /> */}
+            <DateRangePicker
+              startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+              startDateId="startDate" // PropTypes.string.isRequired,
+              endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+              endDateId="endDate" // PropTypes.string.isRequired,
+              onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+              focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+              onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
             />
           </label>
           <br />
