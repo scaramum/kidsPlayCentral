01e4a567333de481f8fb13c97249185d1d23f33f
Mary Scaramuzza <scaramum@gmail.com>
2019-01-26 20:29:37 -0600

    Updated filter functionality

--- src/App.js
+++ src/App.js
@@ -26,16 +26,30 @@ class App extends Component {
     });
   }
   performQuery = () => {
+    var shouldConditions = []
+    if(this.state.location)
+      shouldConditions.push({ "match": { "location":  this.state.location }})
+    if(this.state.age) {
+      shouldConditions.push({ "range": { "minAge": { "lte" : this.state.age}}})
+      shouldConditions.push({ "range": { "maxAge": { "gte" : this.state.age}}})
+    }
+    if(this.startDate && this.state.type!=='daycare')
+      shouldConditions.push({ "range": { "activityStartDate": { "lte" : this.state.startDate}}})
+    if(this.endDate && this.state.type!=='daycare')
+      shouldConditions.push({ "range": { "activityEndDate": { "gte" : this.state.endDate}}})
+    
+    var innerQuery =  {
+      "query": {
+        "bool": {
+          "must": shouldConditions
+        }
+      }
+    }
+
     this.state.client.search({
       index: this.state.type,
       type: this.state.type,
-      body: {
-        query: {
-          match: {
-            "location": this.state.location
-          }
-        }
-      }
+      body: innerQuery
     }).then( (resp) => {
       console.log(resp);
       this.setState({
